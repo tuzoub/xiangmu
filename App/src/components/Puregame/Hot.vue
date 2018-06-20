b234181bd8e8576ee53c033a5a82d15393de4b74
siseae52121 <952413699@qq.com>
2018-06-20 10:21:08 +0800

    newnewnewenwenw

---
+++ App/src/components/Puregame/Hot.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="hot">
+      <div class="tit">
+        <img class="back" src="../../../imgs/back.png" alt="">
+        <div class="text">热门爆款</div>
+        <img class="user" src="../../../imgs/download-5.png" alt="">
+      </div>
+      <Nav></Nav>
+      <div class="con">
+        <div class="item">
+          <img src="../../../imgs/53bc3fdf-d183-4926-8fc1-60c0b673a0d1-1-.jpg" alt="">
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import Nav from "../navcon/Nav"
+    export default {
+        name: "Hot",
+        components:{
+          Nav,
+        }
+    }
+</script>
+
+<style scoped>
+  .hot .tit{
+    width: 100%;
+    height: 58px;
+    border-bottom: 2px solid rgb(215,221,226);
+    line-height: 58px;
+    overflow: hidden;
+
+  }
+  .hot .tit .back{
+    width: 10px;
+    height: 18px;
+    margin-left: 3%;
+    float: left;
+    margin-top: 5%;
+  }
+  .hot .tit .text{
+    width: 87%;
+    font-size:20px;
+    float: left;
+    text-align: center;
+    color:rgb(84, 97, 117);
+  }
+  .hot .tit .user{
+    width: 17px;
+    height: 23px;
+    margin-top: 5%;
+    margin-right: 10px;
+  }
+</style>
